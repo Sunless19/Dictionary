c15866df999483575efa86c299b1cbb9ee03febb
Sunless19 <onofreisergiu70@gmail.com>
2024-03-07 21:54:58 +0200

    Implemented CRUD functionality for images of words Finished Administratory Module for functionality of game Quizz Mode Completed with randomized access to all .xml

---
+++ GuesserGame/DeleteWordDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuesserGame
+{
+    /// <summary>
+    /// Interaction logic for DeleteWordDialog.xaml
+    /// </summary>
+    public partial class DeleteWordDialog : Window
+    {
+        public string WordToDelete { get; private set; }
+
+        public DeleteWordDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            WordToDelete = wordToDeleteTextBox.Text;
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
